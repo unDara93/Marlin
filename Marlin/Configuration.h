ad67af5ba2465d8009cfa649ea9ee1ccd0db8178
unDara93 <mtdr93@gmail.com>
2023-09-05 23:19:20 +0200

    Cambio testa dentata motore più grande

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -956,7 +956,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 40, 40, 200, 48.78, 48.78 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 26.7, 40, 200, 48.78, 48.78 }
 
 /**
  * Default Max Feed Rate (mm/s)
